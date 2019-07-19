9ee0bf66eed032a9f228cffc4e7e5c2e7b5c9d4d
Andrija Radica <andrija.radica@gmail.com>
2019-07-19 19:56:46 +0200

    Add ObservationPlanningAid.py

---
+++ ObservationPlanningAid.py
@@ -0,0 +1,9 @@
+class ObservationPlanningAid:
+    def __init__(self):
+        pass
+
+    def produce_object_list(self, criteria):
+        pass
+
+    def get_ephemeris(self, object_list):
+        pass
